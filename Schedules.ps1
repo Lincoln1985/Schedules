deb2460bb420a9d3f207a0b2f5075c1b1b132dab
Lincoln Oliveira <43508399+Lincoln1985@users.noreply.github.com>
2018-09-22 23:57:37 -0300

    Add files via upload

---
+++ Schedules.ps1
@@ -0,0 +1,16 @@
+﻿# Schedule de bkp
+Clear-Host
+get-command -module psschedulejob | Sort-Object noun
+
+$diario = New-JobTrigger -Daily -at 3am 
+$Semanal = New-JobTrigger -Weekly -DaysOfWeek monday -at 6pm
+$umavez = New-JobTrigger -Once -at (get-date).addhours(1)
+
+
+Register-ScheduledJob -name Backup_Semanal -Trigger $semanal -ScriptBlock {
+Copy-Item F:\Curriculo\*.* F:\Backup -Recurse -Force
+}
+
+Get-ScheduledJob backup_semanal 
+| Unregister-ScheduledJob
+Get-JobTrigger
